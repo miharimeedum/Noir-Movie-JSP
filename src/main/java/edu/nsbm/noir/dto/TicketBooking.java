84010d66b944a9274c2c1bc7a73ae1af5c94c468
oshadi24 <nawanjanaoshadi12@gmail.com>
2024-12-15 23:50:32 +0530

    Impliment TicketBookingt

--- src/main/java/edu/nsbm/noir/dto/TicketBooking.java
+++ src/main/java/edu/nsbm/noir/dto/TicketBooking.java
@@ -1,4 +1,10 @@
 package edu.nsbm.noir.dto;
 
+import edu.nsbm.noir.model.Ticket;
+
 public class TicketBooking {
+    private Integer id;
+    private Integer customerId;
+    private Ticket ticket;
+
 }
--- src/main/java/edu/nsbm/noir/service/impl/TicketServiceImpl.java
+++ src/main/java/edu/nsbm/noir/service/impl/TicketServiceImpl.java
@@ -1,18 +1,42 @@
 package edu.nsbm.noir.service.impl;
 
+import edu.nsbm.noir.entiy.SeatEntity;
 import edu.nsbm.noir.entiy.TicketEntity;
 import edu.nsbm.noir.model.Ticket;
+import edu.nsbm.noir.repository.CoustomerRepository;
+import edu.nsbm.noir.repository.MovieRepository;
+import edu.nsbm.noir.repository.SeatRepository;
 import edu.nsbm.noir.repository.TicketRepository;
 import edu.nsbm.noir.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
 
 @RequiredArgsConstructor
+@Service
 public class TicketServiceImpl implements TicketService {
     private final TicketRepository ticketRepository;
+    private final CoustomerRepository coustomerRepository;
+    private final MovieRepository movieRepository;
+    private final SeatRepository seatRepository;
     private final ModelMapper modelMapper;
+
     @Override
     public void addTicket(Ticket ticket) {
-        ticketRepository.save(modelMapper.map(ticket, TicketEntity.class));
+        if (movieRepository.existsById(ticket.getMovieId())
+                &&seatRepository.existsById(ticket.getSeatId())
+                &&seatRepository.existsById(ticket.getSeatId())
+                &&coustomerRepository.existsById(ticket.getCustomerId())) {
+
+            if(seatRepository.findById(ticket.getSeatId()).get().getIsAvailable()) {
+                ticketRepository.save(modelMapper.map(ticket, TicketEntity.class));
+            }
+        }
     }
 }
+
+/*private Integer id;
+private Integer customerId; fk
+private Integer movieId; fk
+private Integer seatId; fk   */
